519c49405c0971c89f0d48eb7a7b05eed41e419e
Emmanuel Onyeyaforo <rahzy24@gmail.com>
2022-01-17 23:07:02 +0100

    user data updated

--- src/components/screens/PrivateScreen.js
+++ src/components/screens/PrivateScreen.js
@@ -1,10 +1,17 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { Link, useHistory } from "react-router-dom";
 
 
 const PrivateScreen = () => {
   const [error, setError] = useState("");
-  const [privateData, setPrivateData] = useState("");
+  const [students, setStudents] = useState([]);
+  const history = useHistory();
+
+  const logoutUser = () => {
+    localStorage.removeItem("authToken");
+    history.push("/login")
+  }
 
   useEffect(() => {
     const fetchPrivateDate = async () => {
@@ -17,20 +24,29 @@ const PrivateScreen = () => {
 
       try {
         const { data } = await axios.get("http://localhost:4000/api/private", config);
-        setPrivateData(data.data);
-        console.log(data.data)
+        setStudents(data);
+        console.log(data)
       } catch (error) {
         localStorage.removeItem("authToken");
-        setError("You are not authorized please login");
+        setError(`session expired please `);
       }
     };
 
     fetchPrivateDate();
   }, []);
   return error ? (
-    <span className="error-message">{error}</span>
+    <span className="error-message">{error } <Link to="/login">Login</Link></span>
   ) : (
-    <div>{privateData}</div>
+    <div>{students && students.map((student, i) => 
+      <div key={i}>
+        <p>{student._id}</p>
+        <p>{student.username}</p>
+        <p>{student.email}</p>
+        <br />
+      </div>
+    )} 
+    <div onClick={logoutUser}>Logout</div>
+    </div>
   );
 };
 
